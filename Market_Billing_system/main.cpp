118c37d0eb00fddc60036004381d0b0268f5b859
ShamimHossain1 <shamimweb78@gmail.com>
2024-12-24 15:34:09 -0500

    print bill

--- Market_Billing_system/main.cpp
+++ Market_Billing_system/main.cpp
@@ -39,16 +39,21 @@ public:
     }
 };
 
+// **********  Option 1: Add Item Function ****************
+
 void addItem(Bill b)
 {
 
     bool close = false;
     while (!close)
     {
+        system("cls");
         int choice;
+        cout << "\t Add items " << endl;
+        cout << "\t***********" << endl;
         cout << "\t1.Add." << endl;
         cout << "\t2.Close." << endl;
-        cout<<"\tEnter your choice: ";
+        cout << "\tEnter your choice: ";
         cin >> choice;
 
         if (choice == 1)
@@ -56,6 +61,8 @@ void addItem(Bill b)
             system("cls");
             string item;
             int rate, quant;
+            cout << "\t Add items " << endl;
+            cout << "\t***********" << endl;
             cout << "\tEnter item name: ";
             cin >> item;
             cout << "\tEnter rate: ";
@@ -73,21 +80,115 @@ void addItem(Bill b)
             }
             else
             {
-                out << "\t" << b.getItem() << " " << b.getRate() << " " << b.getQuantity() << endl
+                out << "\t" << b.getItem() << " : " << b.getRate() << " : " << b.getQuantity() << endl
                     << endl;
             }
             out.close();
             cout << "\tItem added Successfully!" << endl;
-            Sleep(3000);
+            Sleep(2000);
         }
         else if (choice == 2)
         {
             system("cls");
             close = true;
             cout << "\tBack to main menu..........." << endl;
-            Sleep(3000);
+            Sleep(1000);
         }
     }
+}
+
+//********* */ Option 2: Print Bill Function ****************
+
+void printBill()
+{
+    system("cls");
+    int count = 0;
+    bool close = false;
+    while (!close)
+    {
+        system("cls");
+        int choice;
+        cout << "\t  Print Bill  " << endl;
+        cout << "\t**************" << endl;
+        cout << "\t1. Add Bill" << endl;
+        cout << "\t2. Close Session" << endl;
+        cout << "\tEnter Choice: ";
+        cin >> choice;
+        if (choice == 1)
+        {
+            string item;
+            int quant;
+
+            cout << "\tEnter Item: ";
+            cin >> item;
+            cout << "\tEnter quantity: ";
+            cin >> quant;
+            // b.setItem(item);
+            // b.setQuantity(quant);
+
+            ifstream in("bill.txt");
+            ofstream out("bill_temp.txt");
+            string line;
+            bool found = false;
+            while (getline(in, line))
+            {
+                stringstream ss(line);
+                string itemName;
+                int itemRate, itemQuant;
+                char delimiter;
+                ss >> itemName >> delimiter >> itemRate >> delimiter >> itemQuant;
+                if (item == itemName)
+                {
+                    found = true;
+                    if (quant <= itemQuant)
+                    {
+                        int amount = itemRate * quant;
+                        cout << "\t Item | Rate | Quantity | Amount" << endl;
+                        cout << "\t " << itemName << "\t " << itemRate << "\t " << quant << "\t " << amount << endl;
+                        int newQuant = itemQuant - quant;
+                        itemQuant = newQuant;
+                        count += amount;
+                        
+                        
+                            out <<"\t"<< itemName << " : " << itemRate << " : " << newQuant << endl;
+                        
+                    }
+                    else
+                    {
+                        cout << "\tSorry, insufficient quantity for " << item << "." << endl;
+                        out << line << endl;
+                    }
+                }
+                else
+                {
+                    out << line << endl;
+                }
+               
+            }
+             if (!found)
+                {
+                    cout << "\tItem not available!" << endl;
+                }
+                in.close();
+                out.close();
+                remove("bill.txt");
+                rename("bill_temp.txt", "bill.txt");
+        }
+        else if (choice == 2)
+        {
+
+            close = true;
+            cout << "\tCounting Total Bill" << endl;
+        }
+        Sleep(3000);
+    }
+    system("cls");
+    cout << endl
+         << endl;
+    cout << "\t Total Bill ----------- : " << count << endl
+         << endl;
+    cout << "\t Thank you for shopping with us!" << endl;
+    Sleep(5000);
 }
 
 int main()
@@ -108,8 +209,19 @@ int main()
         cin >> val;
         if (val == 1)
         {
-            system("cls");
+
             addItem(b);
+            Sleep(2000);
+        }
+        else if (val == 2)
+        {
+            printBill();
+        }
+        else if (val == 3)
+        {
+            system("cls");
+            exit = true;
+            cout << "\tThank you for shopping with us!" << endl;
             Sleep(3000);
         }
     }
