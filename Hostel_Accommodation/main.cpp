50ffa60875a32e4007d666c9bd03a78476e0a77f
ShamimHossain1 <shamimweb78@gmail.com>
2025-01-05 00:37:11 -0500

    hostel complete

--- Hostel_Accommodation/main.cpp
+++ Hostel_Accommodation/main.cpp
@@ -31,6 +31,34 @@ public:
     {
         return Bed;
     }
+
+    void reserve()
+    {
+        ifstream in("HostelFile.txt");
+        ofstream out("HostelTempFile.txt");
+        string line;
+        while (getline(in, line))
+        {
+            int pos = line.find("Shera");
+            if (pos != string::npos)
+            {
+                int bed = Bed - 1;
+                Bed = bed;
+                stringstream ss;
+                ss << bed;
+                string strBed = ss.str();
+
+                int bedPos = line.find_last_of(':');
+                line.replace(bedPos + 1, string::npos, strBed);
+            }
+            out << line << endl;
+        }
+        in.close();
+        out.close();
+        remove("HostelFile.txt");
+        rename("HostelTempFile.txt", "HostelFile.txt");
+        cout << "\tBed Reserved Successfully" << endl;
+    }
 };
 
 class Student
@@ -69,41 +97,66 @@ public:
 
 int main()
 {
-    Hostel Shera("Shera", 30000, 100);
-    // ofstream out("StudentFile.txt");
+    Hostel Shera("Shera", 30000, 2);
     ofstream out("HostelFile.txt");
-    out<<"\t"<<Shera.getName()<<" : "<<Shera.getRent()<<" : "<<Shera.getBed()<<endl<<endl;
-    cout<<"Hostel Data Saved"<<endl;
+    out << "\t" << Shera.getName() << " : " << Shera.getRent() << " : " << Shera.getBed() << endl
+        << endl;
+    cout << "Hostel Data Saved" << endl;
     out.close();
 
     Student Sauda;
 
     bool exit = false;
-    while(!exit){
+    while (!exit)
+    {
         system("cls");
         int val;
-        cout<<"\tWelcome To Hostel Accommodation System"<<endl;
-        cout<<"\t**************************************"<<endl;
-        cout<<"\t1.Reserve A Bed."<<endl;
-        cout<<"\t2.Exit."<<endl;
-        cout<<"\tEnter Choice: ";
-        cin>>val;
+        cout << "\tWelcome To Hostel Accommodation System" << endl;
+        cout << "\t**************************************" << endl;
+        cout << "\t1.Reserve A Bed." << endl;
+        cout << "\t2.Exit." << endl;
+        cout << "\tEnter Choice: ";
+        cin >> val;
 
-        if(val ==1){
+        if (val == 1)
+        {
             system("cls");
             string name, rollNo, address;
-            cout<<"\tEnter Your Name: ";
-            cin>>name;
-            cout<<"\tEnter Your Roll No: ";
-            cin>>rollNo;
-            cout<<"\tEnter Your Address: ";
-            cin>>address;
+            cout << "\tEnter Your Name: ";
+            cin >> name;
+            cout << "\tEnter Your Roll No: ";
+            cin >> rollNo;
+            cout << "\tEnter Your Address: ";
+            cin >> address;
 
             Sauda.setName(name);
             Sauda.setRollNo(rollNo);
             Sauda.setAddress(address);
-        }
 
+            if (Shera.getBed() > 0)
+            {
+                Shera.reserve();
+            }
+            else
+            {
+                cout << "\tSorry, No Bed Available." << endl;
+                  exit = true;
+                  Sleep(3000);
+            }
+
+            ofstream out("StudentFile.txt", ios::app);
+            out << "\t" << Sauda.getName() << " : " << Sauda.getRollNo() << " : " << Sauda.getAddress() << endl
+                << endl;
+        }
+        else if(val == 2){
+            cout<<"\tExiting"<<endl;
+            exit = true;
+            Sleep(3000);
+        }
+        else{
+            cout << "\tInvalid Choice. Please Try Again." << endl;
+            Sleep(2000);
+        }
     }
 
     return 0;
