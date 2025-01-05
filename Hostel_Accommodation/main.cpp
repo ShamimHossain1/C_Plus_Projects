4b5fabff1ce02c2b81122ce99d36b817dcce97b5
ShamimHossain1 <shamimweb78@gmail.com>
2025-01-04 23:37:30 -0500

    classes setup

--- Hostel_Accommodation/main.cpp
+++ Hostel_Accommodation/main.cpp
@@ -1,14 +1,74 @@
-#include<iostream>
-#include<fstream>
-#include<sstream>
-#include<Windows.h>
-
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <Windows.h>
 
 using namespace std;
-    
 
-int main(){
+class Hostel
+{
+private:
+    string Name;
+    int Rent, Bed;
 
+public:
+    Hostel(string name, int rent, int bed)
+    {
+        Name = name;
+        Rent = rent;
+        Bed = bed;
+    }
+
+    string getName()
+    {
+        return Name;
+    }
+    int getRent()
+    {
+        return Rent;
+    }
+    int getBed()
+    {
+        return Bed;
+    }
+};
+
+class Student
+{
+
+private:
+    string Name, RollNo, Address;
+
+public:
+    Student() : Name(""), RollNo(""), Address("") {}
+    void setName(string name)
+    {
+        Name = name;
+    }
+    void setRollNo(string rollNo)
+    {
+        RollNo = rollNo;
+    }
+    void setAddress(string address)
+    {
+        Address = address;
+    }
+    string getName()
+    {
+        return Name;
+    }
+    string getRollNo()
+    {
+        return RollNo;
+    }
+    string getAddress()
+    {
+        return Address;
+    }
+};
+
+int main()
+{
 
     return 0;
 }
